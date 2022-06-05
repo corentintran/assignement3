542454a09a8f729b50c131b47aa6064aebbe520f
JayHM0Z <82378903+JayHM0Z@users.noreply.github.com>
2022-06-06 00:11:07 +1000

    Updated MovieCollection.cs

--- MovieCollection.cs
+++ MovieCollection.cs
@@ -37,6 +37,7 @@ public class BTreeNode
 		get { return rchild; }
 		set { rchild = value; }
 	}
+
 }
 
 // invariant: no duplicates in this movie collection
@@ -64,219 +65,144 @@ public class MovieCollection : IMovieCollection
 	// Post-condition: return true if this movie collection is empty; otherwise, return false.
 	public bool IsEmpty()
 	{
-		//To be completed
-		return count == 0;
+		return root == null;
 	}
 
-	private bool InsertRecursive(Movie M, BTreeNode root)
-    {
-		if(M.CompareTo(root.Movie) == 0)
-        {
-			return false;
-        }
-        else if(M.CompareTo(root.Movie) < 0)
-        {
-			if(root.LChild == null)
-            {
-				root.LChild = new BTreeNode(M);
-				count++;
-            }
-            else
-            {
-				InsertRecursive(M, root.LChild);
-            }
-        }
-		else if(root.RChild == null)
-        {
-			root.RChild = new BTreeNode(M);
-			count++;
-        }
-        else
-        {
-			InsertRecursive(M, root.RChild);
-        }
-		return true;
-    }
 	// Insert a movie into this movie collection
 	// Pre-condition: nil
 	// Post-condition: the movie has been added into this movie collection and return true, if the movie is not in this movie collection; otherwise, the movie has not been added into this movie collection and return false.
 	public bool Insert(IMovie movie)
 	{
-		//To be completed
-		if(root == null)
-        {
+		if (root == null) {
 			root = new BTreeNode(movie);
 			count++;
 			return true;
-        }
-		
-		return InsertRecursive((Movie)movie, root);
+		}
+		else if (Search(movie)) return false;
+		else {
+			Insert(movie, root);
+			count++;
+			return true;
+		}
 	}
 
+	// Insert a movie on a BTreeNode
+	// Post-condition: if the BTree node is empty the movie is set on this node
+	// otherwise, the movie is inserted on the left child node or the right child note
+	// according the lexicographic order 
+	private void Insert(IMovie movie, BTreeNode node)
+	{
+		if (node == null) node = new BTreeNode(movie);
+		else {
+			if (movie.CompareTo(node.Movie) < 0) {
+				if (node.LChild == null) node.LChild = new BTreeNode(movie);
+				else Insert(movie, node.LChild);
+			} else {
+				if (node.RChild == null) node.RChild = new BTreeNode(movie);
+				else Insert(movie, node.RChild);
+			}
+		}
+	}
 
-	
 	// Delete a movie from this movie collection
 	// Pre-condition: nil
 	// Post-condition: the movie is removed out of this movie collection and return true, if it is in this movie collection; return false, if it is not in this movie collection
 	public bool Delete(IMovie movie)
 	{
-		//To be completed
-		BTreeNode Ptr = root;
-		BTreeNode Parent = null;
-		BTreeNode P;
-		BTreeNode PP;
-		BTreeNode C;
-		while (Ptr != null && movie.CompareTo(Ptr.Movie) != 0)
-		{
-			Parent = Ptr;
-			if (movie.CompareTo(Ptr.Movie) < 0)
-			{
-				Ptr = Ptr.LChild;
-				
-			}
-			else
-			{
-				Ptr = Ptr.RChild;
-				
-			}
+		BTreeNode node = root;
+		BTreeNode parent = null;
+		while (node != null && movie.CompareTo(node.Movie) != 0){
+			parent = node;
+			if (movie.CompareTo(node.Movie) < 0){ // move to left subtree
+				node = node.LChild;
+			} else { //move to right subtree
+				node = node.RChild;
+			} 
 		}
-		if (Ptr != null)
-		{
-			if (Ptr.LChild != null && Ptr.RChild != null) // 2 Children
-			{
-				if (Ptr.LChild.RChild == null)
-				{
-					Ptr.Movie = Ptr.LChild.Movie;
-					Ptr.LChild = Ptr.LChild.LChild;
-					count--;
-					return true;
-					
-				}
-				else
-				{
-					P = Ptr.LChild;
-					PP = Ptr;
-					while (P.RChild != null)
-					{
-						PP = P;
-						P = P.RChild;
+		if (node != null){ //the search has been successful
+			// case 1 node has 2 children
+			if (node.LChild != null && node.RChild != null){
+				if (node.LChild.RChild == null){
+					node.Movie = node.LChild.Movie;
+					node.LChild = node.LChild.LChild;
+				} else {
+					BTreeNode p = node.LChild;
+					BTreeNode pp = node; //parent of p
+					while (p.RChild != null){
+						pp = p;
+						p = p.RChild;
 					}
-					Ptr.Movie = P.Movie;
-					PP.RChild = P.LChild;
-					count--;
-					return true;
-					
+					node.Movie = p.Movie;
+					pp.RChild = p.LChild; // delete right-most node
+
+				}
+			} else { // case 2 node has no or only one child
+				BTreeNode c = null;
+				if (node.LChild != null) c = node.LChild;
+				else c = node.RChild;
+
+				if (node == root) root = c;
+				else {
+					if (node == parent.LChild){
+						parent.LChild = c;
+					} else {
+						parent.RChild = c;
+					}
 				}
 			}
-			else //No or 1 Child
-			{
-				if (Ptr.LChild != null)
-                {
-					C = Ptr.LChild;
-					count--;
-				}
-                else 
-				{
-					C = Ptr.RChild;
-					count--;
-				}
-
-				if (Ptr == root)
-				{
-					root = C;
-					count--;
-					return true;
-					
-				}
-				else
-				{
-					if (Ptr == Parent.LChild)
-                    {
-						Parent.LChild = C;
-					}
-                    else
-                    {
-						Parent.RChild = C;
-					}
-					count--;
-					return true;
-					
-				}
-
-			}
+			count--;
+			return true;
 		}
 		return false;
-		
 	}
 
 	// Search for a movie in this movie collection
 	// pre: nil
 	// post: return true if the movie is in this movie collection;
 	//	     otherwise, return false.
-	private bool SearchRecursive(Movie M, BTreeNode root)
-    {
-		if(root != null)
-        {
-			if(M.CompareTo(root.Movie) == 0)
-            {
-				return true;
-            }
-            else if(M.CompareTo(root.Movie) < 0)
-            {
-				return SearchRecursive(M, root.RChild);
-            }
-            else
-            {
-				return SearchRecursive(M, root.LChild);
-            }
-        }
-        else
-        {
-			return false;
-        }
-    }
 	public bool Search(IMovie movie)
 	{
-		//To be completed
-		return SearchRecursive((Movie)movie, root);
-        
+		return Search(movie, root);
+	}
+
+	private bool Search(IMovie movie, BTreeNode node)
+	{
+		if (node != null){
+			int compareMovie = movie.CompareTo(node.Movie);
+			if (compareMovie == 0) return true;
+			else if (compareMovie < 0) {
+				return Search(movie, node.LChild);
+			} else {
+				return Search(movie, node.RChild);
+			}
+		} else {
+			return false;
+		}
 	}
 
 	// Search for a movie by its title in this movie collection  
 	// pre: nil
 	// post: return the reference of the movie object if the movie is in this movie collection;
 	//	     otherwise, return null.
-
-	private IMovie SearchRecursiveString(String title, BTreeNode root)
+	public IMovie Search(string movietitle)
 	{
-		if (root != null)
-		{
-			Movie M = new Movie(title);
-			if (M.CompareTo(root.Movie) == 0)
-			{
-				return root.Movie;
+		return Search(movietitle, root);
+	}
+
+	private IMovie Search(string movietitle, BTreeNode node)
+	{
+		if (node != null){
+			int compareMovie = movietitle.CompareTo(node.Movie.Title);
+			if (compareMovie == 0) return node.Movie;
+			else if (compareMovie < 0) {
+				return Search(movietitle, node.LChild);
+			} else {
+				return Search(movietitle, node.RChild);
 			}
-			else if (M.CompareTo(root.Movie) < 0)
-			{
-				return SearchRecursiveString(M.Title, root.RChild);
-			}
-			else
-			{
-				return SearchRecursiveString(M.Title, root.LChild);
-			}
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 	}
-	public IMovie Search(string movietitle)
-	{
-		//To be completed
-		return SearchRecursiveString(movietitle, root);
-	}
-
-
 
 	// Store all the movies in this movie collection in an array in the dictionary order by their titles
 	// Pre-condition: nil
@@ -284,21 +210,31 @@ public class MovieCollection : IMovieCollection
 	public IMovie[] ToArray()
 	{
 		//To be completed
-		IMovie[] Array = new IMovie[0];
-		InOrderTraversal(root, Array);
-		
-		return Array.ToArray();
+		IMovie[] array = new IMovie[count];
+		int index = 0;
+		int finalindex = ToArray(array, root, index);
+		if (finalindex != count) Console.WriteLine("error in ToArray");
+		return array; 
 	}
-	private void InOrderTraversal(BTreeNode root, IMovie[] Array)
-    {
-		if(root != null)
-        {
-			InOrderTraversal(root.LChild, Array);
-			Console.WriteLine(root.Movie.ToString());
-			InOrderTraversal(root.RChild, Array);
-        }
-    }
 
+	// Store all the movie from a BTree Node node to an IMovie Array array
+	// Return the index updated of hoow many movie have been added to the array
+	private int ToArray(IMovie[] array, BTreeNode node, int index)
+	{
+		if (node == null) return index;
+		else {
+			int newindex = index;
+			if (node.LChild != null){
+				newindex = ToArray(array, node.LChild, index);
+			}
+			array[newindex] = node.Movie;
+			newindex++;
+			if (node.RChild != null){
+				newindex = ToArray(array, node.RChild, newindex);
+			}
+			return newindex;	
+		}
+	}
 
 
 	// Clear this movie collection
@@ -306,7 +242,6 @@ public class MovieCollection : IMovieCollection
 	// Post-condition: all the movies have been removed from this movie collection 
 	public void Clear()
 	{
-		//To be completed
 		root = null;
 		count = 0;
 	}
