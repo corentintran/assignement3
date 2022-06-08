471024eb98bc22070f3e2f430c6e120563f1b79c
Mark Gonzales <markg6528@outlook.com>
2022-06-08 21:04:48 +1000

    Revert "more readable console outputs"
    This reverts commit 2acf4b184ccd9199ea93452d6dc431f2f155b604.

--- StaffMenu.cs
+++ StaffMenu.cs
@@ -10,19 +10,12 @@ class StaffMenu
         IMovie movie_reference = Globals.allMovies.Search(movie_title);
         if (movie_reference != null) //the movie is not new
         {
-            //Console output indicating the movie is already in the system and the total copies within the system.
-            Console.WriteLine("The movie already exists in the system. Available copies: {1}, Total copies: {0}", movie_reference.TotalCopies, movie_reference.AvailableCopies);
             //Add the new DVDs
-            Console.WriteLine("How many DVDs do you want to add?");
-           
+            Console.WriteLine("How many DVDs do you want to add ?");
             int newDVDs = Convert.ToInt32(Console.ReadLine());
             movie_reference.AvailableCopies += newDVDs;
             movie_reference.TotalCopies += newDVDs;
 
-            // Console output indicating the processing is a success
-            Console.WriteLine("Successfully added {0} DVDs to {1}. Available copies: {3}, Total copies: {2}", newDVDs, movie_reference.Title, movie_reference.TotalCopies, movie_reference.AvailableCopies);
-            Console.Write("Press enter to continue...");
-            Console.Read();
         }
         else
         { //the movie is new, we need to register the movie in the system
@@ -54,15 +47,8 @@ class StaffMenu
             new_movie.TotalCopies = newDVDs;
 
             //Add the new movie in the movie Collection
-            if (Globals.allMovies.Insert(new_movie))
-            {
-                Console.WriteLine("The movie has been added to the collection! Available copies: {0}, Total Copies: {1}", new_movie.AvailableCopies, new_movie.TotalCopies);
-                Console.Write("Press enter to continue...");
-                Console.Read();
-            }
+            if (Globals.allMovies.Insert(new_movie)) Console.WriteLine("The movie has been added to the collection!");
             else Console.WriteLine("The movie cannot be add to the collection, please try again");
-
-            
         }
     }
 
@@ -78,18 +64,15 @@ class StaffMenu
             if (DVDs_to_remove > movie_reference.TotalCopies)
             //no more DVDs, we remove the movie from the collection
             {
-                Globals.allMovies.Delete(movie_reference);
-                Console.WriteLine("All the DVDs of this movie have been removed, this movie has been removed from the collection");
-                Console.Write("Press enter to continue...");
-                Console.Read();
+                if (Globals.allMovies.Delete(movie_reference))
+                {
+                    Console.WriteLine("All the DVDs of this movie have been removed, this movie has been removed from the collection");
+                }
             }
             else //Remove the new DVDs
             {
                 movie_reference.AvailableCopies -= DVDs_to_remove;
                 movie_reference.TotalCopies -= DVDs_to_remove;
-                Console.WriteLine("{0} DVDs have been removed from the movie: {1}. Available copies: {2}, Total Copies: {3}", DVDs_to_remove, movie_reference.Title, movie_reference.AvailableCopies, movie_reference.TotalCopies);
-                Console.Write("Press enter to continue...");
-                Console.Read();
             }
 
 
@@ -97,8 +80,6 @@ class StaffMenu
         else
         { //the movie is not in the collection
             Console.WriteLine("The movie is not in the collection, please verify the title and try again");
-            Console.Write("Press enter to continue...");
-            Console.Read();
         }
     }
 
@@ -117,8 +98,6 @@ class StaffMenu
         if (Globals.allMembers.Search(new_member))
         {
             Console.WriteLine("The member is already registered!");
-            Console.Write("Press enter to continue...");
-            Console.Read();
             return false;
         }
         else //the member is new, we add him in the collection
@@ -145,10 +124,6 @@ class StaffMenu
             new_member.Pin = psw;
             //Add the member in the collection
             Globals.allMembers.Add(new_member);
-
-            Console.WriteLine("{0} {1} has been registered as a member, with the contact number {2} and password {3}.", new_member.FirstName, new_member.LastName, new_member.ContactNumber, new_member.Pin);
-            Console.Write("Press enter to continue...");
-            Console.Read();
             return true;
         }
     }
@@ -165,8 +140,6 @@ class StaffMenu
         IMember member_to_remove = Globals.allMembers.Find(m);
         if (member_to_remove == null) {
             Console.WriteLine("The member is not registered");
-            Console.Write("Press enter to continue...");
-            Console.Read();
             return false;
         } else {
 
@@ -174,16 +147,9 @@ class StaffMenu
             {
                 //remove the member from the memberCollection
                 Globals.allMembers.Delete(member_to_remove);
-                Console.WriteLine("Member successfully removed.");
-                Console.Write("Press enter to continue...");
-                Console.Read();
                 return true;
             } else  {
-                Console.Write("Removal unsuccessful, member is currently borrowing: ");
-                foreach (IMovie movie in member_to_remove.Borrowings.ToArray()) Console.Write(movie.Title + " ");
-                Console.WriteLine();
-                Console.Write("Press enter to continue...");
-                Console.Read();
+                Console.WriteLine("Impossible to remove member, he is currently renting a movie");
                 return false;
             }
         }
@@ -204,11 +170,8 @@ class StaffMenu
         IMember temp = new Member(first_name, last_name);
         // Reference of
         IMember result = Globals.allMembers.Find(temp);
-        if (result != null) phonenumber = "The contact number of the member is: " + result.ContactNumber;
+        if (result != null) phonenumber = "The contact number of the member is :" + result.ContactNumber;
         else phonenumber = "The member is not registered";
-        Console.WriteLine(phonenumber);
-        Console.Write("Press enter to continue...");
-        Console.Read();
         return phonenumber;
     }
 
@@ -217,17 +180,8 @@ class StaffMenu
         string members_list = "";
         IMovie movie = Globals.allMovies.Search(movie_title);
         //The movie is not registered in the system
-        if (movie == null)
-        {
-            members_list = "The movie is not registered in the system, please check the title";
-        }
-        else
-        {
-            members_list = movie.Borrowers.ToString();
-        }
-        Console.WriteLine(members_list);
-        Console.Write("Press enter to continue...");
-        Console.Read();
+        if (movie == null) members_list = "The movie is not registered in the system, please check the title";
+        else members_list = movie.Borrowers.ToString();
         return members_list;
     }
 }
